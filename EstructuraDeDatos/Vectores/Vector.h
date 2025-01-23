a55b1e4309909055d65c661caea583d68add55c9
AlbertoAmayaSoria <albertoamayasoria@gmail.com>
2025-01-22 18:44:07 -0700

    Vectores como operadores primitivos

--- EstructuraDeDatos/Vectores/Vector.h
+++ EstructuraDeDatos/Vectores/Vector.h
@@ -2,6 +2,7 @@
 #define VECTOR_HPP_INCLUDED
 
 #include <iostream>
+#include <istream>
 #include <ostream>
 
 #define MAX_DIM 10
@@ -10,12 +11,10 @@ class Vector
 {
     friend Vector operator*(double escalar, const Vector v);
     friend std::ostream & operator<<(std::ostream &salida, const Vector v);
+    friend std::istream & operator>>(std::istream &entrada, Vector &v);
 
     public:
-        //Vector();
-        //explicit Vector(int dim);
-        //un constructor de un solo parametro es un operador de convecion del tipo de parametro al tipo de la clase
-        //para evitar esto utilizamos explicit
+        // Un constructor de un solo parametro es un operador de conversion del tipo de parametro al tipo de la clase
         explicit Vector(int dim = MAX_DIM, double val = 0);
 
         int ObtenerDimension() const;
@@ -26,6 +25,8 @@ class Vector
         Vector operator-(const Vector v) const;
         double operator*(const Vector v) const;
         Vector operator*(const double escalar) const;
+        double operator[](int i) const;
+        double & operator[](int i);
         double NormaV() const;
 
     private:

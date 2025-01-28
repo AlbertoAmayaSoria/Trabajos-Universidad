87a4a0eddb6d8d5437a34ead83c3e53b2dbcc5ad
AlbertoAmayaSoria <albertoamayasoria@gmail.com>
2025-01-28 01:04:18 -0700

    28/01/2025

--- EstructuraDeDatos/terminal/Tareas/Tarea1_ClaseMatriz/main.cpp
+++ EstructuraDeDatos/terminal/Tareas/Tarea1_ClaseMatriz/main.cpp
@@ -1,29 +1,62 @@
-#include <iostream>
-#include <ostream>
-
 #include "Matriz.h"
 
-using namespace std;
+int main() {
 
-int main()
-{
-    unsigned int filas, columnas;
+    //falta documentar todo el codigo
 
-    cout << "Programa que realiza operaciones con matrices" << endl << endl;
+    try {
+        unsigned int filas, columnas;
 
-    cout << "Ingrese el numero de columnas de la matriz: " << endl;
+        std::cout << "Ingrese el número de renglones para la matriz 1: ";
+        std::cin >> filas;
+        std::cout << "Ingrese el número de columnas para la matriz 1: ";
+        std::cin >> columnas;
 
-    do{
-        cout << "Ingresa el numero de columnas de la matriz: " << endl;
-        cin >> columnas;
-    }while(columnas < 1);
+        Matriz mat1(filas, columnas);
+        std::cout << "Ingrese los elementos de la matriz 1:" << std::endl;
+        std::cin >> mat1;
 
-    do{
+        std::cout << "Ingrese el número de renglones para la matriz 2: ";
+        std::cin >> filas;
+        std::cout << "Ingrese el número de columnas para la matriz 2: ";
+        std::cin >> columnas;
 
-    }while(filas < 1);
+        Matriz mat2(filas, columnas);
+        std::cout << "Ingrese los elementos de la matriz 2:" << std::endl;
+        std::cin >> mat2;
 
-    cout << filas << " + " << columnas << endl;
-    
+        Matriz suma = mat1 + mat2;
+        //incluir la suma
+        std::cout << "Suma de las matrices:\n" << suma;
+
+        Matriz resta = mat1 - mat2;
+        //incluir la resta
+        std::cout << "Resta de las matrices:\n" << resta;
+
+        Matriz producto = mat1 * mat2;
+        //incluir el producto
+        std::cout << "Producto de las matrices:\n" << producto;
+
+        Matriz producto_escalar = mat1 * 2;
+        //incluir el producto
+        std::cout << "Producto de la matriz por 2:\n" << producto_escalar;
+
+        Matriz trans = mat1.transpuesta();
+        //incluir la matriz original
+        std::cout << "Transpuesta de la matriz 1:\n" << trans;
+
+        mat1.redimensionar(3, 3);
+        //modificar para que el usuario elgia la redimension
+        std::cout << "Matriz 1 redimensionada a 3x3:\n" << mat1;
+
+        //hacer bien la matriz inversa
+
+        /*Matriz inversa = mat1.inversa();
+        std::cout << "Inversa de la matriz 1:\n" << inversa;*/
+
+    } catch (const std::exception& e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
 
     return 0;
 }

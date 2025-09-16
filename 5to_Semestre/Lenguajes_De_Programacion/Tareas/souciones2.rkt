eaafb7aaa935d00338234da004525e3041c2a052
AlbertoAmayaSoria <albertoamayasoria@gmail.com>
2025-09-15 23:58:42 -0700

    ejercicio 1, suma, multiplicacion y factorial, resta bignum

--- 5to_Semestre/Lenguajes_De_Programacion/Tareas/souciones2.rkt
+++ 5to_Semestre/Lenguajes_De_Programacion/Tareas/souciones2.rkt
@@ -17,12 +17,32 @@
     (define successor (lambda (n) (+ n 1)))     ; suma 1
     (define predecessor (lambda (n) (- n 1)))   ; resta 1
 
-    (define plus                                      ;Suma
-      (lambda (int1 int2)                                   ;Elementos a sumar
-        (if (is-zero? int1)                              ;Si x es 0
-            int2                                         ;Devolvemos y
-            (successor (plus (predecessor int1) int2)))))   ;
+    (define plus                                            ; Suma
+      (lambda (int1 int2)                                   ; Elementos a sumar
+        (if (is-zero? int1)                                 ; Si x es 0
+            int2                                            ; Devolvemos y
+            (successor (plus (predecessor int1) int2)))))   ; 
 
-    (define mult
-      (lambda (int1 int2)
-        (...)))
+    ;(define mult referencia de tabulador
+
+    (define mult                                              ; Multipliación
+      (lambda (int1 int2)                                     ; Argumentos a multiplicar
+        (cond
+          ((equal? int1 1) int2)                              ; Multiplicar x por 1 es igual a x
+          ((equal? int2 1) int1)
+          ((equal? int1 0) 0)                                 ; Multiplicar por 0 es 0
+          ((equal? int2 0) 0)
+
+          (else                                               ; Si no
+            (plus (mult(predecessor int1) int2) int2)))))     ; Sumamos int1 veces a int2, recursivamente llamamos a (mult (int1 - 1) int2) hasta que int1 sea igual a 1
+
+    (define factorial                                         ; Factorial
+      (lambda (int)                                           ; Número a aplicar el factorial
+        (cond
+          ((
+          ((equal? int 0) 1)                                  ; El factorial de 0 es 1 caso base
+
+          (else 
+            (mult int [factorial(predecessor int)])))))))     ; Aplicamos recursión sobre la multipliación, (int x (int-1 x (...(1))))
+
+    .

9a37432c4c5dcbd9004a1a3e1108d9e1a1d08f88
Amaya <Albertoamayasoria@gmail.com>
2025-09-21 19:16:17 -0700

    regresion lineal simple/estadistica

---
+++ 5to_Semestre/Estadistica/Regresion_lineal/Sources/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+#include "../Headers/Regresion_lineal.hpp"
+
+using namespace std;
+
+int main(){
+    
+    cout << "Hola mundo" << endl;
+    
+    /*
+     *  Regresión líneal simple para 2 variables
+     */ int cantDatos = 9;
+    int ejeX[cantDatos] = {7, 1, 10, 5, 4, 3, 13, 10, 2};
+    int ejeY[cantDatos] = {2, 9, 2, 5, 7, 11, 2, 5, 14};
+
+    int sum_X = 0;              //  Σx
+    int sum_Y =  0;             //  Σy
+    int sum_XY = 0;             //  Σ(xy)
+    int sum_X_cuadrado = 0;     //  Σ(x^2)
+    int cuadrado_sum_X = 0;     //  (Σx)^2
+    double m = 0;                //  Pendiente
+    double b = 0;                //  Intersección en y
+
+    for(int i = 0; i < cantDatos ; ++i){
+        sum_X = sum_X + ejeX[i];
+        sum_Y = sum_Y + ejeY[i];
+        sum_XY = sum_XY + (ejeX[i] * ejeY[i]);
+        sum_X_cuadrado = ejeX[i] * ejeX[i] + sum_X_cuadrado;
+    }
+    
+    cuadrado_sum_X = sum_X * sum_X;
+
+    m = ((cantDatos * sum_XY) - (sum_X * sum_Y)) / ((cantDatos * sum_X_cuadrado) - cuadrado_sum_X);
+
+    b = (sum_Y - (cantDatos * sum_X)) / cantDatos;
+
+    cout << endl << "ΣX = " << sum_X << " ΣY = " << sum_Y << " ΣXY = " << sum_XY << endl;
+
+    cout << "Σ(xy) = " << sum_XY << " Σ(x^2) = " << sum_X_cuadrado << " (Σx)^2 = " << cuadrado_sum_X << endl;
+
+    cout << "Función: " << m << "x + " << b;
+
+    /*for(int i = 0; i < 10 ; i++){
+        cout << ejeX[i] << " ";
+    }*/
+
+
+
+    return 0;
+}

8071610ec5c62d4e17c3dc61255adba1cc24e27d
Amaya <Albertoamayasoria@gmail.com>
2025-09-22 22:52:01 -0700

    regresion lineal simple para 2 variables, datos predefinidos o por entrada en terminal

--- 5to_Semestre/Estadistica/Regresion_lineal/Sources/main.cpp
+++ 5to_Semestre/Estadistica/Regresion_lineal/Sources/main.cpp
@@ -1,4 +1,6 @@
+#include <cstdlib>
 #include <iostream>
+#include <math.h>
 
 //#include "../Headers/Regresion_lineal.hpp"
 
@@ -6,22 +8,60 @@ using namespace std;
 
 int main(){
     
-    cout << "Hola mundo" << endl;
-    
     /*
      *  Regresión líneal simple para 2 variables
-     */ 
+     *  Datos predefinidos
+     */
     int cantDatos = 9;
     int ejeX[cantDatos] = {7, 1, 10, 5, 4, 3, 13, 10, 2};
-    int ejeY[cantDatos] = {2, 9, 2, 5, 7, 11, 2, 5, 14};
+    int ejeY[cantDatos] = {2, 9, 2, 5, 7, 11, 2, 5, 14};/**/
+    
+    /*
+     *  Regresión lineal simple para 2 variables
+     *  Ingresar datos desde terminal
+     */
 
-    float sum_X = 0;              //  Σx
-    float sum_Y =  0;             //  Σy
-    float sum_XY = 0;             //  Σ(xy)
-    float sum_X_cuadrado = 0;     //  Σ(x^2)
-    float cuadrado_sum_X = 0;     //  (Σx)^2
-    //float /*m*/ pendiente = 0;                //  Pendiente
-    //float /*b*/ interseccion = 0;                //  Intersección en y
+    /*cout << "Cantidad de datos a tratar: ";
+    int cantDatos;
+    cin >> cantDatos;
+
+    float ejeX[cantDatos];
+    float ejeY[cantDatos];
+    for(int i = 0 ; i < cantDatos ; ++i){
+        cout << "X = "; 
+        cin >> ejeX[i];
+        cout << "Y = ";
+        cin >> ejeY[i];
+        cout << "(" << ejeX[i] << ", " << ejeY[i] << ")" << endl;
+    }
+    (1, 2)*/
+
+    cout << endl;
+
+    /*
+     *  Imprimir datos (puntos)
+     */
+
+    for(int i = 0 ; i < cantDatos ; ++i){
+        cout << "(" << ejeX[i] << ", " << ejeY[i] << ")";
+        if(i < (cantDatos - 1)) cout << ", ";
+    }
+
+    /*
+     *  Calculos necesarios
+     */
+
+    float sum_X = 0;                //  Σx
+    float sum_Y =  0;               //  Σy
+    float sum_XY = 0;               //  Σ(xy)
+    float sum_X_cuadrado = 0;       //  Σ(x^2)
+    float cuadrado_sum_X = 0;       //  (Σx)^2
+    float /*m*/ pendiente = 0;      //  Pendiente
+    float /*b*/ interseccion = 0;   //  Intersección en y
+
+    /*
+     * Sumatorias
+     */
 
     for(int i = 0; i < cantDatos ; ++i){
         sum_X = sum_X + ejeX[i];
@@ -32,21 +72,19 @@ int main(){
     
     cuadrado_sum_X = sum_X * sum_X;
 
-    float pendiente = ((cantDatos * sum_XY) - (sum_X * sum_Y)) / ((cantDatos * sum_X_cuadrado) - cuadrado_sum_X);
-      
-    float interseccion = ((sum_Y * sum_X_cuadrado) - (sum_X * sum_XY)) / ((cantDatos * sum_X_cuadrado) - cuadrado_sum_X) /*((sum_Y - (pendiente * sum_X)) / cantDatos)*/;
+    pendiente = ((cantDatos * sum_XY) - (sum_X * sum_Y)) / ((cantDatos * sum_X_cuadrado) - cuadrado_sum_X);
 
-    cout << endl << "ΣX = " << sum_X << " ΣY = " << sum_Y << " ΣXY = " << sum_XY << endl;
+    interseccion = ((sum_Y * sum_X_cuadrado) - (sum_X * sum_XY)) / ((cantDatos * sum_X_cuadrado) - cuadrado_sum_X) /*((sum_Y - (pendiente * sum_X)) / cantDatos)*/;
 
-    cout << "Σ(xy) = " << sum_XY << " Σ(x^2) = " << sum_X_cuadrado << " (Σx)^2 = " << cuadrado_sum_X << " pendiente =" << pendiente << " interseccion = " << interseccion << endl;
+    cout << "\n\nΣx = " << sum_X << ", Σy = " << sum_Y << ", Σxy = " << sum_XY << "\n\n";
 
-    cout << "Función: " << pendiente << "x + " << interseccion << "\n";
+    cout << "Σ(xy) = " << sum_XY << ", Σ(x^2) = " << sum_X_cuadrado << ", (Σx)^2 = " << cuadrado_sum_X << "\n\n";
 
-    /*for(int i = 0; i < 10 ; i++){
-        cout << ejeX[i] << " ";
-    }*/
+    cout << "pendiente = " << pendiente << ", interseccion = " << interseccion << "\n\n";
 
-
+    if(interseccion > 0){
+        cout << "Función: " << pendiente << "x + " << interseccion << "\n";
+    } else cout << "Función: " << pendiente << "x - " << abs(interseccion) << "\n";
 
     return 0;
 }
